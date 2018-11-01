16740d5a28381dadefb9fbe5d9d00f4a23d64b16
Lukáš Hejtmánek <hejtmy@gmail.com>
2018-11-01 18:45:44 +0100

    vewctorises calculation

--- Functions/TimeOperations/timestampstoseconds.m
+++ Functions/TimeOperations/timestampstoseconds.m
@@ -1,8 +1,6 @@
-function [ timestamps ] = timestampstoseconds( timestamps )
+function timestamps = timestampstoseconds( timestamps )
 %TIMESTAMPSTOTIME Summary of this function goes here
 %   Detailed explanation goes here
     TIMESTAMP_SECONDS = 24*60*60;
-    for i = 1:size(timestamps, 1)
-        timestamps(i) = timestamps(i) * TIMESTAMP_SECONDS;
-    end
+    timestamps = timestamps * TIMESTAMP_SECONDS;
 end
